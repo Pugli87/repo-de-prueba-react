74a41b56612ebd61dc340a04fb51cbf8379dacea
Pugli87 <119467014+Pugli87@users.noreply.github.com>
2023-08-01 05:20:24 -0500

    Create MyClassComponent.js

---
+++ src/components/MyClassComponent.js
@@ -0,0 +1,21 @@
+
+import React, { Component } from "react";
+//import styled from 'styled-components';
+//import PropTypes from "prop-types";
+
+
+class MyClassComponent extends Component {
+  static defaultProps = {};
+
+  static propTypes = {};
+
+  render() {
+    return (
+      <div>
+        Class Component
+      </div>
+    );
+  }
+}
+
+export default MyClassComponent
