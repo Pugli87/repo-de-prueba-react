dddb906f3d75d1c81464bc39292b05bc42340955
Pugli87 <119467014+Pugli87@users.noreply.github.com>
2023-08-01 05:19:59 -0500

    Create App.js

---
+++ src/components/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import MyClassComponent from './MyClassComponent';
+import Product from './Product';
+import State from './State';
+import RenderConditional from './RenderConditional';
+import RenderElements from './RenderElements';
+import { EventsES6, EventsES7, MasSobreEventos, Welcome } from './Events';
+import Counter from './Counter';
+import ComunicacionComponente from './ComunicacionComponentes';
+import CicloVida from './CicloVida';
+import { LoginForm, ElementosControlados } from './formularios/Formularios';
+
+const App = () => (
+  <div>
+    <Product name="Cobra" price={1500} />
+    <hr />
+    <MyClassComponent />
+    <hr />
+    <Counter />
+    <hr />
+    <State />
+    <hr />
+    <RenderConditional />
+    <hr />
+    <RenderElements />
+    <hr />
+    <EventsES6 />
+    <hr />
+    <EventsES7 />
+    <hr />
+    <MasSobreEventos />
+    <hr />
+    <Welcome />
+    <hr />
+    <ComunicacionComponente />
+    <hr />
+    <CicloVida />
+    <hr />
+    <LoginForm onSubmit={values => console.log(values)} />
+    <hr />
+    <ElementosControlados />
+  </div>
+);
+
+export default App;
